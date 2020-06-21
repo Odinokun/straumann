908861a05350ae9a6c5a92b2cce6652d3b2e7fb5
odinokun <odinokun@gmail.com>
2020-06-21 21:23:21 +0300

    did implants page

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -15,7 +15,7 @@ module.exports = function() {
     //     // autoplay: true
     //     responsive: [
     //       {
-    //         breakpoint: 766,
+    //         breakpoint: 768,
     //         settings: "unslick"
     //       }
     //     ]
@@ -29,7 +29,7 @@ module.exports = function() {
         arrows: false,
         fade: true,
         infinite: true,
-        autoplay: true
+        // autoplay: true
       });
     }
 
@@ -66,6 +66,47 @@ module.exports = function() {
     nextArrow: "<div class='next'><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"33\" height=\"14\" viewBox=\"0 0 33 14\"><g><g><path d=\"M26.33 13.166l5.623-5.623c.439-.44.439-1.151 0-1.59L26.33.329a1.125 1.125 0 0 0-1.59 1.59l3.702 3.704H1.407a1.125 1.125 0 0 0 0 2.25h27.035l-3.703 3.703a1.121 1.121 0 0 0 0 1.59c.44.44 1.151.44 1.59 0z\"/></g></g></svg></div>"
   });
   //end SEARCH CLINIC
+
+  //begin DOCS SLIDER
+  $('#docs-slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: "<div class='prev'><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"30\" height=\"50\" viewBox=\"0 0 30 50\"><path d=\"M24.845.004l4.394 4.437L8.86 24.64l20.39 20.393-4.42 4.418L0 24.622z\"/></svg></div>",
+    nextArrow: "<div class='next'><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"30\" height=\"50\" viewBox=\"0 0 30 50\"><path d=\"M4.375 49.452L-.02 45.015l20.378-20.201L-.032 4.422 4.388.004 29.22 24.833z\"/></svg></div>",
+    responsive: [
+      {
+        breakpoint: 1241,
+        settings: {
+          slidesToShow: 3,
+        }
+      }, {
+        breakpoint: 941,
+        settings: {
+          slidesToShow: 2,
+        }
+      }, {
+        breakpoint: 641,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1,
+          arrows: false,
+        }
+      }
+    ]
+  });
+  //end DOCS SLIDER
+
+
+
+
+
+
+
+
+
 
 
 
